f7a9c0c17861b1660111e8248d5bbbe3d6450c78
TheUnknownLegend1 <riyaanbakhda2@gmail.com>
2017-07-13 17:36:04 +0530

    Added Question class

--- TestCase.py
+++ TestCase.py
@@ -4,11 +4,35 @@ class TestCase:
         self.inputs = inputs
         self.output = output
 
+questions = list()
+
+class Question:
+    def __init__(self, test_case_list, description, difficulty, number):
+        self.test_case_list = test_case_list
+        self.description = description
+        self.difficulty = difficulty
+        self.number = number
+        global questions
+        questions.append(self)
+
 
 test_cases = list()
-test_cases.append(TestCase("1 4", "5"))
-test_cases.append(TestCase("3 1", "4"))
-test_cases.append(TestCase("8 5", "Exception"))
-test_cases.append(TestCase("6 9", "Exception"))
-test_cases.append(TestCase("2 3", "5"))
-test_cases.append(TestCase("4 0", "4"))
+test_cases.append(TestCase("1\n4", "5"))
+test_cases.append(TestCase("3\n1", "4"))
+test_cases.append(TestCase("8\n5", None))
+test_cases.append(TestCase("6\n9", None))
+test_cases.append(TestCase("2\n3", "5"))
+test_cases.append(TestCase("4\n0", "4"))
+
+Question(test_cases, "Take two inputs as Integers. If sum is greater than 9, throw/raise Exception else print the sum",
+         1, 1)
+
+test_cases = list()
+test_cases.append(TestCase("2", "2 4"))
+test_cases.append(TestCase("3", None))
+test_cases.append(TestCase("4", "2 4 6 8"))
+test_cases.append(TestCase("5", None))
+test_cases.append(TestCase("10", "2 4 6 8 10 12 14 16 18 20"))
+
+Question(test_cases, "Take one input n as an Integer. If n is odd, then throw/raise Exception else print first n even" +
+         "numbers greater than 0", 2, 2)
