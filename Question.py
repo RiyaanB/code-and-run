67ed4fd7840220743a6d7c6bc74b51c8415c7590
TheUnknownLegend1 <riyaanbakhda2@gmail.com>
2017-07-15 17:37:11 +0530

    Postgresql connection

--- Question.py
+++ Question.py
@@ -15,7 +15,7 @@ class Question:
         self.test_cases.append(TestCase(inputs, output))
 
 
-question1 = Question("Beginner's question", "Input two integers. If the sum is greater than 9, throw an Exception," +
+question1 = Question("Beginners question", "Input two integers. If the sum is greater than 9, throw an Exception," +
                      " else print the sum")
 
 question1.add_case("1\n2", "3")
@@ -23,3 +23,12 @@ question1.add_case("4\n5", "9")
 question1.add_case("10\n12", "null")
 question1.add_case("3\n8", "null")
 question1.add_case("-10\n20", "null")
+
+question2 = Question("Even & Odd", "Input Integer N. If N is even, throw an exception else print the first N even " +
+                                   "numbers greater than 0")
+
+question2.add_case("3", "2\n4\n6")
+question2.add_case("4", "null")
+question2.add_case("5", "2\n4\n6\n8\n10")
+question2.add_case("2", "null")
+question2.add_case("7", "2\n4\n6\n8\n10\n12\n14")
