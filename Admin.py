c267594713a2422091a589becb1b59e82ab42437
TheUnknownLegend1 <riyaanbakhda2@gmail.com>
2017-07-15 21:50:41 +0530

    Added more demo questions and replaced listbox button with click listener

--- Admin.py
+++ Admin.py
@@ -6,4 +6,5 @@ if __name__ == "__main__":
     database_connection.create_table()
     database_connection.insert_question(question1)
     database_connection.insert_question(question2)
-
+    database_connection.insert_question(question3)
+    database_connection.insert_question(question4)
